5552f7f2b8f4a3f5cb9b6846816f4a8a128ae7f0
pranav-salunkhe <pvsalunkhe2003@gmail.com>
2024-08-28 00:10:36 +0530

    fix unauthorized url issue

--- lib/config/conductor.ts
+++ lib/config/conductor.ts
@@ -1,8 +1,8 @@
 import { orkesConductorClient } from "@io-orkes/conductor-javascript";
 
 export const conductorClientPromise = orkesConductorClient({
-    keyId: "35fbeb99-072c-4380-8456-327b5feb60d7",
-    keySecret: "YpDI6sxhJjSKyTsrWoYCtc5GJUR12k6PcIj7M28fet9NaDHT",
+    keyId: process.env.NEXT_PUBLIC_CONDUCTOR_ID,
+    keySecret: process.env.NEXT_PUBLIC_CONDUCTOR_SECRET,
     serverUrl: "https://play.orkes.io/api",
 })
 
